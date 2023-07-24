0f43cd637dcf63fa7d54e6696c9c2172e1a22623
Cod-Am <amrit.672004@gmail.com>
2023-07-24 08:50:08 +0530

    layout is done

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,7 +6,13 @@ const Banner = () => {
   return (
     <div className='banner'>
         {/* Tiger */}
-        <div className='tiger'>Tiger</div>
+        <div className='tiger_content'>
+            <div className='tiger'>Tiger</div>
+            <div className='discription'>
+            The tiger (Panthera tigris) is the largest living cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange fur with a white underside.
+            </div>
+        </div>
+        
         <div><img src={tigerImage} alt='' className='image'/></div>
         {/* Elephant */}
         {/* Zebra */}
--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Nav.css'
+import logo from './img/logo.png'
 const Nav = () => {
   return (
     <div className='Nav'>
       <div className='logo'>
-        logo
+        <img src={logo} alt='' className='logo'/>
       </div>
       <div className='title'>Animalia Encyclopedia</div>
       <span />
