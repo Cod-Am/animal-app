1f0e846815886e6703c62d1c2da62f9959c95ea3
Cod-Am <amrit.672004@gmail.com>
2023-07-24 11:13:26 +0530

    added hr tag to the banner for styling

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,11 +7,28 @@ const Banner = () => {
     <div className='banner'>
         {/* Tiger */}
         <div className='tiger_content'>
-        
-          <div className='tiger'>Tiger</div>
-          <div className='discription'>
-            The tiger (Panthera tigris) is the largest living cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange fur with a white underside.
+          {/* Fixed Text:Species */}
+          <div className='text-with-line'>
+          <span className='species'>SPECIES</span>
+          <hr className='line'/>
           </div>
+
+          {/* Species Name */}
+          <div className='tiger'>South China Tiger</div>
+          
+          {/* Fixed Text : Status */}
+          <div>
+            <span className='status'>STATUS</span>
+            <hr className='line'/>
+          </div>
+
+          {/* Status of the species */}
+          <div className='status_detail'>Critically Endangered</div>
+          
+          {/* Non functional Section */}
+          {/* <div className='discription'>
+            The tiger (Panthera tigris) is the largest living cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange fur with a white underside.
+          </div> */}
         </div>
         
         <div><img src={tigerImage} alt='' className='image'/></div>
