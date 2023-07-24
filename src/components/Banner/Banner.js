6d81a4439a9c86b603e544af1236ab5c07090a20
Cod-Am <amrit.672004@gmail.com>
2023-07-24 12:14:17 +0530

    layout done now, next is the image animation

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,35 +5,35 @@ import tigerImage from './img/Tiger.png';
 const Banner = () => {
   return (
     <div className='banner'>
-        {/* Tiger */}
-        <div className='tiger_content'>
-          {/* Fixed Text:Species */}
-          <div className='text-with-line'>
-          <span className='species'>SPECIES</span>
-          <hr className='line'/>
-          </div>
+      {/* Tiger */}
+      <div className='tiger_content'>
+        {/* Fixed Text:Species */}
+        <span className='text-with-line'>
+          <span className='species '>SPECIES</span>
+          <hr className='line margin-species-hr' />
+        </span>
 
-          {/* Species Name */}
-          <div className='tiger'>South China Tiger</div>
-          
-          {/* Fixed Text : Status */}
-          <div>
-            <span className='status'>STATUS</span>
-            <hr className='line'/>
-          </div>
+        {/* Species Name */}
+        <div className='tiger'>South China Tiger</div>
 
-          {/* Status of the species */}
-          <div className='status_detail'>Critically Endangered</div>
-          
-          {/* Non functional Section */}
-          {/* <div className='discription'>
+        {/* Fixed Text : Status */}
+        <span className='text-with-line'>
+          <span className='status'>STATUS</span>
+          <hr className='line margin-status-hr' />
+        </span>
+
+        {/* Status of the species */}
+        <div className='status_detail'>Critically Endangered</div>
+
+        {/* Non functional Section */}
+        {/* <div className='discription'>
             The tiger (Panthera tigris) is the largest living cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange fur with a white underside.
           </div> */}
-        </div>
-        
-        <div><img src={tigerImage} alt='' className='image'/></div>
-        {/* Elephant */}
-        {/* Zebra */}
+      </div>
+
+      <div><img src={tigerImage} alt='' className='image' /></div>
+      {/* Elephant */}
+      {/* Zebra */}
     </div>
   );
 };
