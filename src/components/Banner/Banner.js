46840f01a2e11b1bcc0fd93a6364c6f9a19a3863
Cod-Am <amrit.672004@gmail.com>
2023-07-24 09:52:18 +0530

    set up a nav bar below containg species, etc.

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,10 +7,11 @@ const Banner = () => {
     <div className='banner'>
         {/* Tiger */}
         <div className='tiger_content'>
-            <div className='tiger'>Tiger</div>
-            <div className='discription'>
+        
+          <div className='tiger'>Tiger</div>
+          <div className='discription'>
             The tiger (Panthera tigris) is the largest living cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange fur with a white underside.
-            </div>
+          </div>
         </div>
         
         <div><img src={tigerImage} alt='' className='image'/></div>
--- src/components/navbar/Nav.js
+++ src/components/navbar/Nav.js
@@ -3,12 +3,19 @@ import './Nav.css'
 import logo from './img/logo.png'
 const Nav = () => {
   return (
-    <div className='Nav'>
-      <div className='logo'>
-        <img src={logo} alt='' className='logo'/>
+    <div>
+      <div className='Nav'>
+        <div className='logo'>
+          <img src={logo} alt='' className='logo' />
+        </div>
+        <div className='title'>Animalia Encyclopedia</div>
       </div>
-      <div className='title'>Animalia Encyclopedia</div>
-      <span />
+      <div className='nav_content'>
+        <div>Species</div>
+        <div>About Us</div>
+        <div>Our Work</div>
+        <div>Get Involved</div>
+      </div>
     </div>
   )
 }
